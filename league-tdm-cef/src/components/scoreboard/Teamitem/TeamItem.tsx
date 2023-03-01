a6b7a69bb5fd4751ca443a3bbaed3d25c603130c
Andrjuha-joma-jozhik <andrey.zhmudenko1@gmail.com>
2023-03-01 22:31:45 +0200

    scoreboard

---
+++ league-tdm-cef/src/components/scoreboard/Teamitem/TeamItem.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import cl from 'classnames'
+import s from './TeamItem.module.sass'
+
+interface Props {
+  children?: | JSX.Element | JSX.Element | React.ReactNode;
+  side: string;
+}
+
+const TeamItem: FC<Props> = ( {children, side} ) => {
+
+  return (
+    <div className={cl(s.teamItem, side === 'left' ? s.left : s.right)}>
+      {children}
+    </div>
+  )
+}
+
+export default TeamItem
