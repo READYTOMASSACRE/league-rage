e5ff61f9b729899011304048ab841eb34ae75131
Andrjuha-joma-jozhik <andrey.zhmudenko1@gmail.com>
2023-03-02 08:59:48 +0200

    delete cell

--- league-tdm-cef/src/components/scoreboard/PlayerItem/PlayerItem.tsx
+++ league-tdm-cef/src/components/scoreboard/PlayerItem/PlayerItem.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react'
 import cl from 'classnames'
 import s from './PlayerItem.module.sass'
 import { IPlayers } from '../../../types'
-import Cell from '../Cell/Cell';
 
 interface Props {
   player: IPlayers;
@@ -14,13 +13,7 @@ const PlayerItem: FC<Props> = ({ player, currentPlayer, position }) => {
 
   return (
     <div className={s.playerItem}>
-      <Cell value={position} width={'22px'} display={'flex'} ai={'center'} jc={'center'}/>
-      <Cell value={player.name} padding={'0 0 0 5px'}  ai={'center'} fg={1}/>
-      <Cell value={player.kills} width={'33px'} display={'flex'} ai={'center'} jc={'center'}/>
-      <Cell value={player.assists} width={'33px'} display={'flex'} ai={'center'} jc={'center'}/>
-      <Cell value={player.death} width={'33px'} display={'flex'} ai={'center'} jc={'center'}/>
-      <Cell value={player.ping} width={'33px'} display={'flex'} ai={'center'} jc={'center'}/>
-        {/* <div className={s.position}>
+        <div className={s.position}>
           {position}
         </div>
         <div className={cl(s.name, currentPlayer && s.current)}>
@@ -37,7 +30,7 @@ const PlayerItem: FC<Props> = ({ player, currentPlayer, position }) => {
         </div>
         <div className={s.ping}>
           {player.ping}
-        </div> */}
+        </div>
     </div>
   )
 }
