e548d3e772e46f3412172a5bbc70940f1d585b88
rtmm <ixerr@mail.ru>
2023-03-04 04:37:36 +0500

    code style

--- league-tdm-cef/src/index.tsx
+++ league-tdm-cef/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import {Scoreboard} from './components/scoreboard'
+import { Scoreboard } from './components/scoreboard'
 
 const App = () => {
   return (
