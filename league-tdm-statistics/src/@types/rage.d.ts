e5db7f77c2dc79e35ada58843373c202e5dbb7f0
here cacao <ixerr@mail.ru>
2023-03-26 02:12:32 +0500

    fix typings

--- league-tdm-statistics/src/@types/rage.d.ts
+++ league-tdm-statistics/src/@types/rage.d.ts
@@ -2,10 +2,6 @@ declare global {
   interface PlayerMp {
     userId: string
   }
-  interface DummyMp {
-    setVariable(key: string, value: any): void
-    getVariable(key: string): any
-  }
 }
 
 export {}
