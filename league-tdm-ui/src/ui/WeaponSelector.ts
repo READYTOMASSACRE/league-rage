0a044c0f2a1fd1e3b5635d63cc7f0b41e7b192ce
here cacao <ixerr@mail.ru>
2023-04-10 20:16:26 +0500

    fix weapon close on round end, round remove or death

--- league-tdm-ui/src/ui/WeaponSelector.ts
+++ league-tdm-ui/src/ui/WeaponSelector.ts
@@ -37,6 +37,13 @@ export default class WeaponRequest {
 
     this.request(true)
   }
+
+  @event([Events["tdm.vote.end"], Events["tdm.round.remove"], "playerDeath"])
+  close() {
+    if (this.visible) {
+      return this.request(false)
+    }
+  }
   
   @event(Events["tdm.weapon.request"])
   request(visible?: boolean) {
