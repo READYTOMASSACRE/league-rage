6f93f6e2b8edcec545aa793713ba72788f85f87b
here cacao <ixerr@mail.ru>
2023-04-27 14:32:45 +0500

    fix mongodb repository

--- server/statistics/src/repository/mongodb/MongodbRepository.ts
+++ server/statistics/src/repository/mongodb/MongodbRepository.ts
@@ -2,6 +2,8 @@ import { MongoEntity } from "../../@types";
 import BaseRepository from "../BaseRepository";
 import { Db, Collection, Filter, OptionalUnlessRequiredId } from "mongodb";
 import { maxLimit } from "../../helpers";
+import { userId } from "../../../../../core/src/types";
+import { ObjectId } from "mongodb";
 
 export default abstract class MongodbRepository<T extends MongoEntity> extends BaseRepository<T, Db> {
   constructor(db: Db) {
@@ -9,26 +11,34 @@ export default abstract class MongodbRepository<T extends MongoEntity> extends B
   }
 
   async save(t: T) {
-    const item = await this.getById(t._id)
+    const {_id, ...updateDoc} = t
+    const item = await this.getById(_id)
     if (item) {
       const {_id, ...doc} = item
 
-      await this.collection.updateOne(<Filter<T>>{_id}, {$set: {...doc, ...t}})
+      await this.collection.updateOne(<Filter<T>>{_id}, {$set: {
+        ...doc,
+        ...<T>updateDoc,
+      }})
     } else {
-      await this.collection.insertOne(<OptionalUnlessRequiredId<T>>t)
+      await this.collection.insertOne(<OptionalUnlessRequiredId<T>>updateDoc)
     }
   }
 
-  async get({ ids, limit = maxLimit, offset, ...query }: Filter<T> = {}) {
+  async get({ ids = [], limit = maxLimit, offset, ...query }: Filter<T> = {}) {
+    ids = ids.map((id: userId) => new ObjectId(id))
+
     return this.collection.find({
-      ...(ids ? { _id: { $in: ids } } : false),
+      ...(ids.length ? { _id: { $in: ids } } : false),
       ...query,
     }).limit(Number(limit)).skip(Number(offset) || 0).sort({ _id: -1 }).toArray() as any as T[]
   }
 
-  async count({ ids, ...query }: Filter<T> = {}) {
+  async count({ ids = [], ...query }: Filter<T> = {}) {
+    ids = ids.map((id: userId) => new ObjectId(id))
+
     return this.collection.countDocuments({
-      ...(ids ? { _id: { $in: ids } } : false),
+      ...(ids.length ? { _id: { $in: ids } } : false),
       ...query,
     })
   }
@@ -37,8 +47,8 @@ export default abstract class MongodbRepository<T extends MongoEntity> extends B
     return this.collection.findOne(query) as any as T
   }
 
-  async getById(_id: number | string) {
-    return this.collection.findOne(<Filter<T>>{_id}) as any as T
+  async getById(_id: userId) {
+    return this.collection.findOne(<Filter<T>>{_id: new ObjectId(_id)}) as any as T
   }
 
   get collection(): Collection<T> {
